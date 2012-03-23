33434ef0f979321a7850bcc88ee62aef7154de72
kashiwasan <pc.kashiwasan@gmail.com>
2012-03-23 19:09:43 +0900

    first commit

---
+++ apps/pc_frontend/modules/testGadget/actions/components.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the OpenPNE package.
+ * (c) OpenPNE Project (http://www.openpne.jp/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file and the NOTICE file that were distributed with this source code.
+ */
+
+class memberComponents extends opMemberComponents
+{
+  public function executeSmtLayoutHomeTest($request)
+  {
+    
+  }
+
+  public function executeSmtLayoutMemberTest($request)
+  {
+
+  }
+
+  public function executeSmtLayoutGroupTest($request)
+  {
+
+  }
+}
---
+++ apps/pc_frontend/modules/testGadget/templates/_smtLayoutHomeTest.php
@@ -0,0 +1,9 @@
+<?php use_javascript('/opTestGadgetPlugin/web/js/jquery.js') ?>
+<?php use_javascript('/opTestGadgetPlugin/web/js/qunit/qunit.js') ?>
+<?php use_stylesheet('/opTestGadgetPlugin/web/js/qunit/qunit.css') ?>
+<?php include_javascript() ?>
+
+<script type="text/javascript">
+$(document).ready(function(){
+});
+</script>
